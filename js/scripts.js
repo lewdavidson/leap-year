f788eec11ac8467ce34af9ba3a3dd4d862f94d5f
Lew <lewdavidson47@gmail.com>
2017-10-25 09:01:44 -0700

    add user interface, with seperation between it and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
+/* biz logic */
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
 
 };
-
-
-
+/* user interface */
 $(document).ready(function(){
   $("form#leapForm").submit(function(event){
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#answer").text(result);
-  });
 
+    $(".year").text(year);
+
+    if (result === false) { /* or !result */
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#answer").show();
+  });
 });
